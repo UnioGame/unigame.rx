97e6ca32060d7f4559d42fccf9b7869f90634a18
Mefodei <mefodei.link@gmail.com>
2020-10-07 20:20:52 +0300

    - update package

---
+++ Runtime/Extensions/ExObservableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniModules.UniGame.Rx.Runtime.Operations;
+using UniRx;
+using UnityEngine.UI;
+
+namespace UniModules.UniGame.Rx.Runtime.Extensions
+{
+    public static class ExObservableExtensions
+    {
+    
+        public static IObservable<T> ToChain<T>(this IObservable<T> source, IObservable<Unit> skipSource = null)
+        {
+            return new ChainObservable<T>(source, skipSource);
+        }
+
+        public static IObservable<T> AddToChain<T>(this IObservable<T> source, IObservable<T> other)
+        {
+            if (source is ChainObservable<T> chainObservable) {
+                return chainObservable.Add(other);
+            }
+
+            return ToChain(source).AddToChain(other);
+        }
+    }
+}
