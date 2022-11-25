b27731030044c766b37aed9175a23964240b4372
Mefodei <mefodei.link@gmail.com>
2022-11-25 14:45:27 +0600

    - update rx extensions

--- Runtime/Extensions/RxBindExtensions.cs
+++ Runtime/Extensions/RxBindExtensions.cs
@@ -1,163 +1,13 @@
-using System;
-using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
-using TMPro;
-using UniModules.UniCore.Runtime.Utils;
-using UniGame.Core.Runtime;
-using UniModules.UniUiSystem.Runtime.Utils;
-using UniRx;
-using UnityEngine;
-using UnityEngine.UI;
-
-namespace UniModules.Rx.Extensions
+namespace UniGame.Rx.Runtime.Extensions
 {
+    using System;
+    using System.Collections.Generic;
+    using Cysharp.Threading.Tasks;
+    using UniGame.Core.Runtime;
+    using UniRx;
+    
     public static class RxBindExtensions
     {
-
-        #region ugui
-        
-
-        public static TView Bind<TView,TValue>(this TView sender, IObservable<TValue> source, Button command)
-            where TView : ILifeTimeContext
-        {
-            if (command == null) return sender;
-            return Bind(sender,source,x => command.onClick?.Invoke());
-        }
-
-        public static TView Bind<TView>(this TView sender, Button source, Action<Unit> command, int throttleTime = 0)
-            where TView : ILifeTimeContext
-        {
-            return source == null ? sender : sender.Bind(source, () => command(Unit.Default), throttleTime);
-        }
-        
-        public static TView Bind<TView>(this TView sender, Button source, Action<Unit> command, TimeSpan throttleTime)
-            where TView : ILifeTimeContext
-        {
-            return source == null ? sender : sender.Bind(source, () => command(Unit.Default), throttleTime);
-        }
-
-        public static TView Bind<TView>(this TView sender, Button source, Action command,TimeSpan throttleTime)
-            where TView : ILifeTimeContext
-        {
-            if (!source) return sender;
-
-            var clickObservable = throttleTime.TotalMilliseconds <= 0
-                ? source.OnClickAsObservable()
-                : source.OnClickAsObservable().ThrottleFirst(throttleTime);
-
-            return Bind(sender, clickObservable, command);
-        }
-
-        public static TView Bind<TView>(this TView view, IObservable<bool> source, Button button)
-            where TView : ILifeTimeContext
-        {
-            return !button ? view : view.Bind(source, x => button.interactable = x);
-        }
-        
-        public static TView Bind<TView>(this TView view, Button source, IReactiveCommand<Unit> command, int throttleInMilliseconds = 0)
-            where TView : ILifeTimeContext
-        {
-            return !source ? view : Bind(view, source, () => command.Execute(Unit.Default), TimeSpan.FromMilliseconds(throttleInMilliseconds));
-        }
-        
-        public static TView Bind<TView>(this TView view, IObservable<Unit> source, IReactiveCommand<Unit> command)
-                    where TView : ILifeTimeContext
-        {
-            return Bind(view, source, x => command.Execute(Unit.Default));
-        }
-        
-        public static TView Bind<TView>(this TView sender, Button source, Action command,int throttleInMilliseconds = 0)
-            where TView : ILifeTimeContext
-        {
-            return Bind(sender, source, command,TimeSpan.FromMilliseconds(throttleInMilliseconds));
-        }
-
-                        
-        public static TView Bind<TView>(this TView view, IObservable<Sprite> source, Button button, int frameThrottle = 0)
-            where TView : ILifeTimeContext
-        {
-            if (!button || !button.image)
-                return view;
-            
-            return view.Bind(source, x => button.image.SetValue(x));
-        }
-        
-        
-        public static TView Bind<TView>(this TView view, IObservable<bool> source, Toggle toggle)
-            where TView : ILifeTimeContext
-        {
-            return !toggle ? view : Bind(view,source, x => toggle.isOn = x);
-        }
-        
-        public static TSource Bind<TSource>(this TSource view, Toggle source, IReactiveProperty<bool> value)
-            where TSource : ILifeTimeContext
-        {
-            return !source ? view : Bind(view,source.OnValueChangedAsObservable(), value);
-        }
-
-        public static TView Bind<TView>(this TView view, Toggle source, Action<bool> value)
-            where TView : ILifeTimeContext
-        {
-            return !source ? view : view.Bind(source.OnValueChangedAsObservable(), value);
-        }
-
-        public static TView Bind<TView>(this TView view, IObservable<bool> source, CanvasGroup group)
-            where TView : ILifeTimeContext
-        {
-            if (!group) return view;
-            return view.Bind(source,x => group.interactable = x);
-        }
-        
-        public static TView Bind<TView>(this TView view, IObservable<Sprite> source, Image image)
-            where TView : ILifeTimeContext
-        {
-            return !image ? view : view.Bind(source.Where(x => x!=null), x => image.SetValue(x) );
-        }
-        
-        public static TView Bind<TView>(this TView view, IObservable<string> source, TextMeshProUGUI text)
-            where TView : ILifeTimeContext
-        {
-            return view.Bind(source,x => text.SetValue(x));
-        }
-        
-        public static TView Bind<TView>(this TView view, IObservable<int> source, TextMeshProUGUI text)
-            where TView : ILifeTimeContext
-        {
-            return view.Bind(source, x => text.SetValue(x.ToStringFromCache()));
-        }
-        
-        public static TView Bind<TView,TValue>(this TView view,
-            IObservable<TValue> source,
-            Func<TValue,string> format, TextMeshProUGUI text)
-            where           TView : ILifeTimeContext
-        {
-            var stringObservable = source.Select(format);
-            return view.Bind(stringObservable, text);
-        }
-        
-        public static TView Bind<TView>(this TView view, IObservable<string> source, TMP_Text text)
-            where TView : ILifeTimeContext
-        {
-            return !text ? view : view.Bind(source,x => text.SetValue(x));
-        }
-
-        public static TView Bind<TView>(this TView view, IObservable<string> source, TextMeshPro text)
-            where TView : ILifeTimeContext
-        {
-            if (!text) return view;
-            return view.Bind(source, x => text.text = x);
-        }
-
-        public static TView Bind<TView>(this TView view, IObservable<int> source, TextMeshPro text)
-            where TView : ILifeTimeContext
-        {
-            return view.Bind(source, x => text.text = x.ToStringFromCache());
-        }
-
-        
-        #endregion
-        
-        
         #region lifetime context
         
         public static TView Bind<TView,TValue>(this TView view, IObservable<TValue> source, IObserver<Unit> observer)
@@ -183,6 +33,13 @@ namespace UniModules.Rx.Extensions
             where T : ILifeTimeContext
         {
             return Bind<T,TValue>(sender, source, action, sender.LifeTime);
+        }
+        
+        public static T Bind<T>(this T sender, IDisposable disposable)
+            where T : ILifeTimeContext
+        {
+            sender.LifeTime.AddDispose(disposable);
+            return sender;
         }
         
         public static T Bind<T, TValue>(this T sender, IObservable<TValue> source, Action<T,TValue> action)
---
+++ Runtime/Ui/RxUiExtensions.cs
@@ -0,0 +1,159 @@
+namespace UniGame.Rx.Runtime.Extensions
+{
+    using Extensions;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using System;
+    using TMPro;
+    using UniGame.Core.Runtime;
+    using UniModules.UniCore.Runtime.Utils;
+    using UniModules.UniUiSystem.Runtime.Utils;
+    using UniRx;
+    
+    public static class RxUiExtensions
+    {
+        
+        #region ugui
+
+        public static TView Bind<TView,TValue>(this TView sender, IObservable<TValue> source, Button command)
+            where TView : ILifeTimeContext
+        {
+            if (command == null) return sender;
+            return sender.Bind(source,x => command.onClick?.Invoke());
+        }
+
+        public static TView Bind<TView>(this TView sender, Button source, Action<Unit> command, int throttleTime = 0)
+            where TView : ILifeTimeContext
+        {
+            return source == null ? sender : sender.Bind(source, () => command(Unit.Default), throttleTime);
+        }
+        
+        public static TView Bind<TView>(this TView sender, Button source, Action<Unit> command, TimeSpan throttleTime)
+            where TView : ILifeTimeContext
+        {
+            return source == null ? sender : sender.Bind(source, () => command(Unit.Default), throttleTime);
+        }
+
+        public static TView Bind<TView>(this TView sender, Button source, Action command,TimeSpan throttleTime)
+            where TView : ILifeTimeContext
+        {
+            if (!source) return sender;
+
+            var clickObservable = throttleTime.TotalMilliseconds <= 0
+                ? source.OnClickAsObservable()
+                : source.OnClickAsObservable().ThrottleFirst(throttleTime);
+
+            return sender.Bind(clickObservable, command);
+        }
+
+        public static TView Bind<TView>(this TView view, IObservable<bool> source, Button button)
+            where TView : ILifeTimeContext
+        {
+            return !button ? view : view.Bind(source, x => button.interactable = x);
+        }
+        
+        public static TView Bind<TView>(this TView view, Button source, IReactiveCommand<Unit> command, int throttleInMilliseconds = 0)
+            where TView : ILifeTimeContext
+        {
+            return !source ? view : Bind(view, source, () => command.Execute(Unit.Default), TimeSpan.FromMilliseconds(throttleInMilliseconds));
+        }
+        
+        public static TView Bind<TView>(this TView view, IObservable<Unit> source, IReactiveCommand<Unit> command)
+                    where TView : ILifeTimeContext
+        {
+            return view.Bind(source, x => command.Execute(Unit.Default));
+        }
+        
+        public static TView Bind<TView>(this TView sender, Button source, Action command,int throttleInMilliseconds = 0)
+            where TView : ILifeTimeContext
+        {
+            return Bind(sender, source, command,TimeSpan.FromMilliseconds(throttleInMilliseconds));
+        }
+
+                        
+        public static TView Bind<TView>(this TView view, IObservable<Sprite> source, Button button, int frameThrottle = 0)
+            where TView : ILifeTimeContext
+        {
+            if (!button || !button.image)
+                return view;
+            
+            return view.Bind(source, x => button.image.SetValue(x));
+        }
+        
+        
+        public static TView Bind<TView>(this TView view, IObservable<bool> source, Toggle toggle)
+            where TView : ILifeTimeContext
+        {
+            return !toggle ? view : view.Bind(source, x => toggle.isOn = x);
+        }
+        
+        public static TSource Bind<TSource>(this TSource view, Toggle source, IReactiveProperty<bool> value)
+            where TSource : ILifeTimeContext
+        {
+            return !source ? view : view.Bind(source.OnValueChangedAsObservable(), value);
+        }
+
+        public static TView Bind<TView>(this TView view, Toggle source, Action<bool> value)
+            where TView : ILifeTimeContext
+        {
+            return !source ? view : view.Bind(source.OnValueChangedAsObservable(), value);
+        }
+
+        public static TView Bind<TView>(this TView view, IObservable<bool> source, CanvasGroup group)
+            where TView : ILifeTimeContext
+        {
+            if (!group) return view;
+            return view.Bind(source,x => group.interactable = x);
+        }
+        
+        public static TView Bind<TView>(this TView view, IObservable<Sprite> source, Image image)
+            where TView : ILifeTimeContext
+        {
+            return !image ? view : view.Bind(source.Where(x => x!=null), x => image.SetValue(x) );
+        }
+
+        #endregion
+        
+        public static TView Bind<TView>(this TView view, IObservable<string> source, TextMeshProUGUI text)
+            where TView : ILifeTimeContext
+        {
+            return view.Bind(source,x => text.SetValue(x));
+        }
+        
+        public static TView Bind<TView>(this TView view, IObservable<int> source, TextMeshProUGUI text)
+            where TView : ILifeTimeContext
+        {
+            return view.Bind(source, x => text.SetValue(x.ToStringFromCache()));
+        }
+        
+        public static TView Bind<TView,TValue>(this TView view,
+            IObservable<TValue> source,
+            Func<TValue,string> format, TextMeshProUGUI text)
+            where           TView : ILifeTimeContext
+        {
+            var stringObservable = source.Select(format);
+            return view.Bind(stringObservable, text);
+        }
+        
+        public static TView Bind<TView>(this TView view, IObservable<string> source, TMP_Text text)
+            where TView : ILifeTimeContext
+        {
+            return !text ? view : view.Bind(source,x => text.SetValue(x));
+        }
+
+        public static TView Bind<TView>(this TView view, IObservable<string> source, TextMeshPro text)
+            where TView : ILifeTimeContext
+        {
+            if (!text) return view;
+            return view.Bind(source, x => text.text = x);
+        }
+
+        public static TView Bind<TView>(this TView view, IObservable<int> source, TextMeshPro text)
+            where TView : ILifeTimeContext
+        {
+            return view.Bind(source, x => text.text = x.ToStringFromCache());
+        }
+        
+        
+    }
+}
